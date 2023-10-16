180915b3fc56d4b05dba4caaec0aeccb10889a7c
RyunosukeFunasako <e205732@ie.u-ryukyu.ac.jp>
2023-10-16 23:26:53 +0900

    add: TODOタスクの削除機能の追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,36 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [inputTodo, setInputTodo] = useState("");
   const [uncompleteTodos, setUncompleteTodos] = useState(["ああああ", "いいいい"]);
-  const [completeTodos, setCompleteTodos] = useState(["うううう"])
+  const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+  const onChangeInput = (event) => setInputTodo(event.target.value);
+  const onClickAddButton = () => {
+    if (inputTodo === "") return;
+    const newUncompleteTodos = [...uncompleteTodos, inputTodo];
+    setUncompleteTodos(newUncompleteTodos);
+    setInputTodo("");
+  };
+  const onClickDeleteButton = (index) => {
+    const newUncompleteTodos = [...uncompleteTodos];
+    newUncompleteTodos.splice(index, 1);
+    setUncompleteTodos(newUncompleteTodos);
+  };
   return (
     <>
        <div className="input-area">
-        <input type="text" placeholder="TODOを入あああ力" />
-        <button>追加</button>
+        <input type="text" placeholder="TODOを入力" value={inputTodo} onChange={onChangeInput} />
+        <button onClick={onClickAddButton}>追加</button>
        </div>
        <div className="uncomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {uncompleteTodos.map((todo) => {
+          {uncompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="todo">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDeleteButton(index)}>削除</button>
               </div>     
             )
           })}
