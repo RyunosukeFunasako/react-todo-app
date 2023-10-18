a2939b16acaa2a19f824cd1036024504e74b7912
RyunosukeFunasako <e205732@ie.u-ryukyu.ac.jp>
2023-10-18 21:37:10 +0900

    clean: css-in-js

--- src/components/UncompleteTodos.jsx
+++ src/components/UncompleteTodos.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "violet",
+  width: "400px",
+  minHeight: "200px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+};
+
 export const UncompleteTodos = (props) => {
   const { uncompleteTodos, onClickCompleteButton, onClickDeleteButton } = props;
   return (
-    <div className="uncomplete-area">
+    <div style={style}>
       <p className="title">未完了のTODO</p>
       <ul>
         {uncompleteTodos.map((todo, index) => {
